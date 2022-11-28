5ca218c23d78c643c8d83f667abd7cfd4b8ce5fc
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2022-11-29 00:56:26 +1000

    initial commit

---
+++ src/main/java/pro/sky/telegram_bot_pitomnik/TelegramBotPitomnikApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegram_bot_pitomnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotPitomnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotPitomnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/telegram_bot_pitomnik/TelegramBotPitomnikApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegram_bot_pitomnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotPitomnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
